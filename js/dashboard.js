15303b06eb9564c9b7f488b3e9a6fa7545a1a3f3
YiZH <zhihai.yi@genlot.com>
2020-09-14 20:18:20 +0800

    代码优化

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,143 +1,174 @@
-var dashboard = function(options) {
-  var doc = document
-  var canvas = doc.querySelector(options.el)
-  var ctx = canvas.getContext("2d")
-  var dpr = window.devicePixelRatio || 1
-  var width = options.width * dpr
-  var height = options.height * dpr
-  canvas.setAttribute('width', width)
-  canvas.setAttribute('height', height)
-  
-  // 绘制圆弧
-  // 中心点坐标
-  var x = width / 2
-  var y = height / 2
-  // 半径
-  var r = width / 2 * 0.95
-  // 线条宽度
-  var lineWidth = 8 * dpr
-  ctx.lineWidth = lineWidth
-
-  // 结束角度
-  var endAngle = 220
-
-  // 指示球半径
-  var br = lineWidth * 1.3
-
-  // 指针绘制半径
-  var prr = width / 2 * 0.75
-
+var Dashboard = {
+  create: function(params) {
+    var self = this,
+    doc = document,
+    canvas = doc.querySelector(params.el),
+    ctx = canvas.getContext("2d"),
+    dpr = window.devicePixelRatio || 1,
+    width = params.width * dpr,
+    height = params.height * dpr,
+    color = params.color,
+    // 绘制圆弧
+    // 中心点坐标
+    x = width / 2,
+    y = height / 2,
+    // 半径
+    r = width / 2 * 0.95,
+    // 线条宽度
+    lineWidth = 8 * dpr,
+    // 开始角度
+    startAngle = 160,
+    // 结束角度
+    endAngle = 220,
+    // 指示球半径
+    br = lineWidth * 1.3,
+    // 指针缩放值
+    pointerScale = Math.min(params.pointer.scale, 1) * dpr,
+    // 指针绘制位置半径
+    prr = width / 2 * 0.75,
+    maxValue = params.maxValue,
+    currentValue = Math.min(params.currentValue, maxValue),
+    highlightEndAngle = Math.max(0, currentValue) / maxValue * endAngle,
+    pointerImg = new Image(),
+    dashboardParams = {
+      ctx: ctx,
+      x: x,
+      y: y,
+      r: r,
+      dpr: dpr,
+      width: width,
+      height: height,
+      color: color,
+      lineWidth: lineWidth,
+      startAngle: startAngle,
+      endAngle: endAngle,
+      br: br,
+      pointerScale: pointerScale,
+      prr: prr,
+      maxValue: maxValue,
+      currentValue: currentValue,
+      highlightEndAngle: highlightEndAngle,
+      pointerImg: pointerImg,
+      update: params.update
+    };
+    
+    canvas.setAttribute('width', width)
+    canvas.setAttribute('height', height)
+    pointerImg.src = params.pointer.src
+    pointerImg.onload = function () {
+      self.animation(params.duration, dashboardParams)
+    }
+  },
   // 动画算法
-  function quadEaseOut(t, b, c, d) {
+  quadEaseOut: function (t, b, c, d) {
     return -c *(t /= d) * (t - 2) + b
-  }
-
-  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
-  var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame
-
-
+  },
   // 动画函数
-  function animation(duration) {
-    var t = 0
-    var run = function() {
-      ctx.clearRect(0, 0, width, height)
+  animation: function(duration, params) {
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame,
+    cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame,
+    self = this,
+    ctx = params.ctx,
+    t = 0,
+    maxValue = params.maxValue,
+    highlightEndAngle = params.highlightEndAngle,
+    endAngle = params.endAngle,
+    run = function() {
+      ctx.clearRect(0, 0, params.width, params.height)
       t += 5
       var mt = Math.min(t, duration) // 时间不能超过动画时长
-      var stepAngle = quadEaseOut(mt, 0, highlightEndAngle, duration)
+      var stepAngle = self.quadEaseOut(mt, 0, highlightEndAngle, duration)
       // 绘制背景弧
-      drawArc(0.2, endAngle)
+      self.drawArc(0.2, endAngle, params)
       // 绘制高亮弧
-      drawArc(1, stepAngle)
+      self.drawArc(1, stepAngle, params)
       // 绘制高亮球
-      drawBall(stepAngle)
+      self.drawBall(stepAngle, params)
       // 绘制指针
-      drawPointer(stepAngle)
+      self.drawPointer(stepAngle, params)
       // 绘制刻度背景
       var count = 31
       var cAngle = endAngle / count
       
-      drawCalibration(0.2, count, -cAngle)
+      self.drawCalibration(0.2, count, -cAngle, params)
       // 绘制高亮刻度
-      drawCalibration(1, quadEaseOut(mt, 0, Math.ceil(highlightEndAngle / cAngle), duration),  -cAngle)
+      self.drawCalibration(1, self.quadEaseOut(mt, 0, Math.ceil(highlightEndAngle / cAngle), duration),  -cAngle, params)
       
       if (t > duration) {
         cancelAnimationFrame(run)
         return
       }
-      if (typeof options.update === 'function') {
+      if (typeof params.update === 'function') {
         // 更新仪表值
-        options.update((stepAngle / endAngle * options.maxValue).toFixed(2))
+        params.update((stepAngle / endAngle * maxValue).toFixed(2))
       }
       requestAnimationFrame(run)
-    }
+    };
     run()
-  }
-
-
-  function drawArc(opacity, angle) {
+  },
+  drawArc: function(opacity, angle, params) {
+    var ctx = params.ctx
     ctx.save()
-    ctx.translate(x, y)
-    ctx.strokeStyle = options.color
+    ctx.lineWidth = params.lineWidth
+    ctx.translate(params.x, params.y)
+    ctx.strokeStyle = params.color
     ctx.globalAlpha = opacity || 1
     ctx.beginPath()
-    ctx.arc(0, 0, (width * 0.95 - 10 * dpr) / 2, 160 * Math.PI / 180, (angle + 160) * Math.PI / 180, false)
+    ctx.arc(0, 0, (params.width * 0.95 - 10 * params.dpr) / 2, params.startAngle * Math.PI / 180, (angle + params.startAngle) * Math.PI / 180, false)
     ctx.stroke()
     ctx.restore()
-  }
-  
-
-  // 绘制高亮弧
-  var currentValue = Math.min(options.currentValue, options.maxValue)
-  var highlightEndAngle = Math.max(0, currentValue) / options.maxValue * endAngle
-  
-
+  },
   // 绘制刻度
-  function drawCalibration(opacity, total, angle) {
-    var w = 10 * dpr
+  drawCalibration: function(opacity, total, angle, params) {
+    var w = 10 * params.dpr,
+    ctx = params.ctx,
+    x = params.x,
+    y = params.y,
+    endAngle = params.endAngle,
+    r = params.r;
     for(var i = 0; i < total; i++) {
       var deg = ((angle * i - (360 - endAngle - angle) / 2) * Math.PI / 180)
       ctx.save()
       ctx.translate(x + r * 0.9 * Math.sin(deg), y + r * 0.9* Math.cos(deg))
       ctx.rotate((((360 - endAngle - angle) / 2) + 90 - angle * i) * Math.PI / 180)
-      ctx.fillStyle = options.color
+      ctx.fillStyle = params.color
       ctx.globalAlpha = opacity || 1
       ctx.fillRect(-w / 2, -w / 4, w, w / 2);
       ctx.restore()
     }
-  }
-
-
+  },
   // 绘制圆球
-  function drawBall(angle) {
-    var bx = x + (r - br / 2) * Math.sin(-((angle + (360 - endAngle) / 2) * Math.PI / 180))
-    var by = y + (r - br / 2) * Math.cos(-((angle + (360 - endAngle) / 2) * Math.PI / 180))
+  drawBall: function(angle, params) {
+    var ctx = params.ctx,
+    x = params.x,
+    y = params.y,
+    br = params.br,
+    endAngle = params.endAngle,
+    r = params.r,
+    bx = x + (r - br / 2) * Math.sin(-((angle + (360 - endAngle) / 2) * Math.PI / 180)),
+    by = y + (r - br / 2) * Math.cos(-((angle + (360 - endAngle) / 2) * Math.PI / 180));
     ctx.save()
     ctx.beginPath()
     ctx.arc(bx, by, br, 0, 2 * Math.PI)
-    ctx.fillStyle = options.color
+    ctx.fillStyle = params.color
     ctx.fill()
     ctx.restore()
-  }
-
-
+  },
   // 绘制指针
-  function drawPointer(angle) {
-    var scale = Math.min(options.pointer.scale, 1) * dpr
-    var deg = -((angle + (360 - endAngle) / 2) * Math.PI / 180)
+  drawPointer: function(angle, params) {
+    var ctx = params.ctx,
+    x = params.x,
+    y = params.y,
+    prr = params.prr,
+    pointerImg = params.pointerImg,
+    endAngle = params.endAngle,
+    pointerScale = params.pointerScale,
+    deg = -((angle + (360 - endAngle) / 2) * Math.PI / 180);
     ctx.save()
-    ctx.translate(x + (prr - img.naturalWidth * scale / 2) * Math.sin(deg), y + (prr - img.naturalHeight * scale / 2) * Math.cos(deg))
+    ctx.translate(x + (prr - pointerImg.naturalWidth * pointerScale / 2) * Math.sin(deg), y + (prr - pointerImg.naturalHeight * pointerScale / 2) * Math.cos(deg))
     ctx.rotate((((360 - endAngle) / 2) + angle) * Math.PI / 180)
-    ctx.scale(scale, scale)
-    ctx.drawImage(img, -img.naturalWidth / 2, -img.naturalHeight / 2)
+    ctx.scale(pointerScale, pointerScale)
+    ctx.drawImage(pointerImg, -pointerImg.naturalWidth / 2, -pointerImg.naturalHeight / 2)
     ctx.restore()
   }
-
-  
-
-  var img = new Image()
-  img.src = options.pointer.src
-  img.onload = function () {
-    animation(options.duration)
-  }
-  
 }
